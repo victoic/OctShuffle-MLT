a5f4e51be4caed7e9f4ad0162232950c5510d9c8
Victor Lundgren <victor15_toic@hotmail.com>
2019-05-21 16:01:55 -0300

    Changes to model

--- models.py
+++ models.py
@@ -10,7 +10,6 @@ import torch.nn.functional as F
 import numpy as np
 
 from octconv import OctConv2d
-from shufflenet import ShuffleNet
 from torch.nn import LeakyReLU, Conv2d, Dropout2d, LogSoftmax, InstanceNorm2d
 
 import math    
@@ -207,6 +206,184 @@ def dice_loss(inp, target):
   return - ((2. * intersection + smooth) /
             (iflat.sum() + tflat.sum() + smooth))
 
+def _avg_pool2d(input, kernel_size, stride=None, padding=0, ceil_mode=False, count_include_pad=True):
+  #hf, lf = input
+  #CHANGE TO F.interpolate()
+  #hf = F.avg_pool2d(hf, kernel_size=kernel_size, stride=stride, padding=padding, ceil_mode=ceil_mode, count_include_pad=count_include_pad)
+  #lf = F.avg_pool2d(lf, kernel_size=kernel_size, stride=stride, padding=padding, ceil_mode=ceil_mode, count_include_pad=count_include_pad)
+  return _downsample2x(input)
+
+def _downsample2x(input):
+  hf, lf = input
+  new_hf_dim = int(hf.shape[2]/2), int(hf.shape[3]/2)
+  new_lf_dim = int(lf.shape[2]/2), int(lf.shape[3]/2)
+  hf = F.interpolate(hf, new_hf_dim, mode='bilinear', align_corners=True)
+  lf = F.interpolate(lf, new_lf_dim, mode='bilinear', align_corners=True)
+  return hf, lf
+
+def conv3x3(in_channels, out_channels, stride=1, 
+          padding=1, bias=True, groups=1, alpha=0.5):    
+  """3x3 convolution with padding
+  """
+  return OctConv2d(
+    in_channels, 
+    out_channels, 
+    kernel_size=3, 
+    stride=stride,
+    padding=padding,
+    bias=bias,
+    groups=groups,
+    alpha=alpha)
+
+
+def conv1x1(in_channels, out_channels, groups=1, alpha=0.5):
+  """1x1 convolution with padding
+  - Normal pointwise convolution When groups == 1
+  - Grouped pointwise convolution when groups > 1
+  """
+  return OctConv2d(
+    in_channels, 
+    out_channels, 
+    kernel_size=1, 
+    groups=groups,
+    stride=1,
+    alpha=alpha)
+
+
+def channel_shuffle(x, groups):
+  hf, lf = x
+  batchsize_hf, num_channels_hf, height_hf, width_hf = hf.data.size()
+  batchsize_lf, num_channels_lf, height_lf, width_lf = lf.data.size()
+
+  channels_per_group_hf = num_channels_hf // groups
+  channels_per_group_lf = num_channels_lf // groups
+  
+  # reshape
+  hf = hf.view(batchsize_hf, groups, 
+    channels_per_group_hf, height_hf, width_hf)
+  lf = lf.view(batchsize_lf, groups, 
+    channels_per_group_lf, height_lf, width_lf)
+
+  # transpose
+  # - contiguous() required if transpose() is used before view().
+  #   See https://github.com/pytorch/pytorch/issues/764
+  hf = torch.transpose(hf, 1, 2).contiguous()
+  lf = torch.transpose(lf, 1, 2).contiguous()
+
+  # flatten
+  hf = hf.view(batchsize_hf, -1, height_hf, width_hf)
+  lf = lf.view(batchsize_lf, -1, height_lf, width_lf)
+
+  x = hf, lf
+  return x
+
+class ShuffleUnit(nn.Module):
+  def __init__(self, in_channels, out_channels, groups=3,
+               grouped_conv=True, combine='add', alpha=0.5):
+      
+    super(ShuffleUnit, self).__init__()
+
+    self.in_channels = in_channels
+    self.out_channels = out_channels
+    self.grouped_conv = grouped_conv
+    self.combine = combine
+    self.groups = groups
+    self.bottleneck_channels = self.out_channels // 4
+
+    # define the type of ShuffleUnit
+    if self.combine == 'add':
+      # ShuffleUnit Figure 2b
+      self.depthwise_stride = 1
+      self._combine_func = self._add
+    elif self.combine == 'concat':
+      # ShuffleUnit Figure 2c
+      self.depthwise_stride = 2
+      self._combine_func = self._concat
+      
+      # ensure output of concat has the same channels as 
+      # original output channels.
+      self.out_channels -= self.in_channels
+    else:
+      raise ValueError("Cannot combine tensors with \"{}\"" \
+                       "Only \"add\" and \"concat\" are" \
+                       "supported".format(self.combine))
+
+    # Use a 1x1 grouped or non-grouped convolution to reduce input channels
+    # to bottleneck channels, as in a ResNet bottleneck module.
+    # NOTE: Do not use group convolution for the first conv1x1 in Stage 2.
+    self.first_1x1_groups = self.groups if grouped_conv else 1
+
+    self.g_conv_1x1_compress = self._make_grouped_conv1x1(
+      self.in_channels,
+      self.bottleneck_channels,
+      self.first_1x1_groups,
+      batch_norm=True,
+      relu=True
+      )
+
+    # 3x3 depthwise convolution followed by batch normalization
+    self.depthwise_conv3x3 = conv3x3(
+      self.bottleneck_channels, self.bottleneck_channels,
+      stride=self.depthwise_stride, groups=self.bottleneck_channels)
+    self.bn_after_depthwise = _BatchNorm2d(self.bottleneck_channels)
+
+    # Use 1x1 grouped convolution to expand from 
+    # bottleneck_channels to out_channels
+    self.g_conv_1x1_expand = self._make_grouped_conv1x1(
+      self.bottleneck_channels,
+      self.out_channels,
+      self.groups,
+      batch_norm=True,
+      relu=False
+      )
+
+  @staticmethod
+  def _add(x, out):
+    # residual connection
+    return x[0] + out[0], x[1] + out[1]
+
+  @staticmethod
+  def _concat(x, out):
+    _cat = _Cat()
+    # concatenate along channel axis
+    return _cat((x,out), 1)
+
+  def _make_grouped_conv1x1(self, in_channels, out_channels, groups,
+    batch_norm=True, relu=False):
+
+    modules = OrderedDict()
+
+    conv = conv1x1(in_channels, out_channels, groups=groups)
+    modules['conv1x1'] = conv
+
+    if batch_norm:
+      modules['batch_norm'] = _BatchNorm2d(out_channels)
+    if relu:
+      modules['relu'] = _ReLU()
+    if len(modules) > 1:
+      return nn.Sequential(modules)
+    else:
+      return conv
+
+
+  def forward(self, x):
+    # save for combining later with output
+    residual = x
+
+    if self.combine == 'concat':
+      residual = _avg_pool2d(residual, kernel_size=3, 
+        stride=2, padding=1)
+
+    out = self.g_conv_1x1_compress(x)
+    out = channel_shuffle(out, self.groups)
+    out = self.depthwise_conv3x3(out)
+    out = self.bn_after_depthwise(out)
+    out = self.g_conv_1x1_expand(out)
+    
+    out = self._combine_func(residual, out)
+    out = F.relu(out[0]), F.relu(out[1])
+    return out
+
 class BasicBlockSep(nn.Module):
   expansion = 1
   def __init__(self, inplanes, planes, stride=1, downsample=None, dilation=1, alpha=0.5):
@@ -389,21 +566,13 @@ class ModelResNetSep2(nn.Module):
     self.max2 = _MaxPool2d((2, 1), stride=(2,1))
     self.leaky = _LeakyReLU(negative_slope=0.01, inplace=True)
     self.leaky2 = LeakyReLU(negative_slope=0.01, inplace=True)
-    
-    self.layer1 = self._make_layer(BasicBlockIn, 24, 3, stride=1)
-    self.inplanes = 64
-    self.layer2 = self._make_layer(BasicBlockIn, 128, 4, stride=2, alpha=alpha)
-    self.layer3 = self._make_layer(BasicBlockSepIn, 256, 6, stride=2, alpha=alpha)
-    self.layer4 = self._make_layer(BasicBlockSepIn, 512, 4, stride=2, alpha=alpha)
-    
-    self.feature4 = OctConv2d(512, 256, 1, stride=1, padding=0, bias=False, alpha=(0.5, 0))
-    self.feature3 = OctConv2d(256, 256, 1, stride=1, padding=0, bias=False, alpha=(0.5, 0))
-    self.feature2 = OctConv2d(128, 256, 1, stride=1, padding=0, bias=False, alpha=(0.5, 0))
 
-    shufflenet = ShuffleNet()
-    self.layer2 = shufflenet.stage2
-    self.layer3 = shufflenet.stage3
-    self.layer4 = shufflenet.stage4
+    self.groups = 3
+    self.stage_out_channels = [-1, 24, 240, 480, 960]
+    self.stage_repeats = [3, 7, 3]
+    self.layer2 = self._make_stage(2)
+    self.layer3 = self._make_stage(3)
+    self.layer4 = self._make_stage(4)
 
     self.feature4 = OctConv2d(960, 256, 1, stride=1, padding=0, bias=False, alpha=(0.5, 0))
     self.feature3 = OctConv2d(480, 256, 1, stride=1, padding=0, bias=False, alpha=(0.5, 0))
@@ -672,15 +841,15 @@ class ModelMLTRCTW(nn.Module):
     self.leaky = _LeakyReLU(negative_slope=0.01, inplace=True)
     self.leaky2 = LeakyReLU(negative_slope=0.01, inplace=True)
     
-    self.layer1 = self._make_layer(BasicBlockIn, 64, 3, stride=1, alpha=alpha)
-    self.inplanes = 64
-    self.layer2 = self._make_layer(BasicBlockIn, 128, 4, stride=2, alpha=alpha)
-    self.layer3 = self._make_layer(BasicBlockSepIn, 256, 6, stride=2, alpha=alpha)
-    self.layer4 = self._make_layer(BasicBlockSepIn, 512, 4, stride=2, alpha=alpha)
+    #self.layer1 = self._make_layer(BasicBlockIn, 64, 3, stride=1, alpha=alpha)
+    #self.inplanes = 64
+    #self.layer2 = self._make_layer(BasicBlockIn, 128, 4, stride=2, alpha=alpha)
+    #self.layer3 = self._make_layer(BasicBlockSepIn, 256, 6, stride=2, alpha=alpha)
+    #self.layer4 = self._make_layer(BasicBlockSepIn, 512, 4, stride=2, alpha=alpha)
     
-    self.feature4 = OctConv2d(512, 256, 1, stride=1, padding=0, bias=False, alpha=(0.5, 0))
-    self.feature3 = OctConv2d(256, 256, 1, stride=1, padding=0, bias=False, alpha=(0.5, 0))
-    self.feature2 = OctConv2d(128, 256, 1, stride=1, padding=0, bias=False, alpha=(0.5, 0))
+    #self.feature4 = OctConv2d(512, 256, 1, stride=1, padding=0, bias=False, alpha=(0.5, 0))
+    #self.feature3 = OctConv2d(256, 256, 1, stride=1, padding=0, bias=False, alpha=(0.5, 0))
+    #self.feature2 = OctConv2d(128, 256, 1, stride=1, padding=0, bias=False, alpha=(0.5, 0))
     
     self.upconv2 = conv_dw_plain(256, 256, stride=1, alpha=0)
     self.upconv1 = conv_dw_plain(256, 256, stride=1, alpha=0)
@@ -718,25 +887,39 @@ class ModelMLTRCTW(nn.Module):
     import copy
     self.layer0o = copy.deepcopy(self.layer0)
     self.layer0_1o = copy.deepcopy(self.layer0_1)
-  
-  def _make_layer(self, block, planes, blocks, stride=1, alpha=0.5):
+
+  def _make_stage(self, stage):
+    modules = OrderedDict()
+    stage_name = "ShuffleUnit_Stage{}".format(stage)
     
-    downsample = None
-    if stride != 1 or self.inplanes != planes * block.expansion:
-      downsample = nn.Sequential(
-  
-        OctConv2d(self.inplanes, planes * block.expansion,
-                  kernel_size=1, stride=stride, bias=False, alpha=alpha),
-        _BatchNorm2d(planes * block.expansion, alpha_in=alpha, alpha_out=alpha),
+    # First ShuffleUnit in the stage
+    # 1. non-grouped 1x1 convolution (i.e. pointwise convolution)
+    #   is used in Stage 2. Group convolutions used everywhere else.
+    grouped_conv = stage > 2
+    
+    # 2. concatenation unit is always used.
+    first_module = ShuffleUnit(
+      self.stage_out_channels[stage-1],
+      self.stage_out_channels[stage],
+      groups=self.groups,
+      grouped_conv=grouped_conv,
+      combine='concat'
       )
+    modules[stage_name+"_0"] = first_module
 
-    layers = []
-    layers.append(block(self.inplanes, planes, stride, downsample))
-    self.inplanes = planes * block.expansion
-    for i in range(1, blocks):
-      layers.append(block(self.inplanes, planes, alpha=alpha))
+    # add more ShuffleUnits depending on pre-defined number of repeats
+    for i in range(self.stage_repeats[stage-2]):
+      name = stage_name + "_{}".format(i+1)
+      module = ShuffleUnit(
+        self.stage_out_channels[stage],
+        self.stage_out_channels[stage],
+        groups=self.groups,
+        grouped_conv=True,
+        combine='add'
+        )
+      modules[name] = module
 
-    return nn.Sequential(*layers)
+    return nn.Sequential(modules)
   
   def forward_ocr(self, x):
     

ae94e1f53ce41296244cc699c29edf7f577afa9c
Victor Lundgren <victor15_toic@hotmail.com>
2019-09-09 11:27:29 -0300

    Fixed channel division

--- octconv/octconv.py
+++ octconv/octconv.py
@@ -5,14 +5,14 @@ from torch import mul
 class GatedConv(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, bias=False, dilation=1, groups=1, activation=nn.Sigmoid()):
         super(GatedConv, self).__init__()
-        self.half_channels = int(in_channels/2)
+        self.half_channels = int(out_channels/2)
         self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride=stride, padding=padding, bias=bias, dilation=dilation, groups=groups)
         self.activation_1 = activation
 
     def forward(self, x):
-        print(x.shape)
         print(self.half_channels)
         x = self.conv(x)
+        print(x.shape)
         x_1 = x[:,0:self.half_channels,:,:]
         print(x_1.shape)
         x_1 = self.activation_1(x_1)
